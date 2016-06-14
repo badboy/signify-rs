8d6a259fca433846172943dc6ab3a08c615f2bf6
Jan-Erik Rediger <janerik@fnordig.de>
2016-06-14 21:05:10 +0200

    chore: Add travis, tests and metadata

---
+++ tests/full-cycle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTPATH=$(readlink -f $(dirname $0))
+cd $SCRIPTPATH
+
+PUB=$(mktemp pub.$$.XXXXXXXXXX)
+PRIV=$(mktemp priv.$$.XXXXXXXXXX)
+MSG=$(mktemp msg.$$.XXXXXXXXXX)
+
+cleanup() {
+  rm $PUB $PRIV $MSG $MSG.sig
+}
+
+trap cleanup SIGHUP SIGINT SIGTERM EXIT
+
+cargo run -- -G -p $PUB -s $PRIV
+head -c 100 /dev/urandom > $MSG
+cargo run -- -S -s $PRIV -m $MSG -x ${MSG}.sig
+cargo run -- -V -p $PUB -m $MSG -x ${MSG}.sig
